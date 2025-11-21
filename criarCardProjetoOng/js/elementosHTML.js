f44dd275f6e176918f29929beeeb03108bb82bca
roseEstudos <selesrosilene@gmail.com>
2025-11-20 21:15:58 -0300

    Subindo as telas: Cadastro e Login da ONG

---
+++ criarCardProjetoOng/js/elementosHTML.js
@@ -0,0 +1,32 @@
+const imagemUpload = document.getElementById('imageUpload')
+const imagemPreview = document.getElementById('imgSubida')
+const textUpload = document.querySelector('.textUpload')
+const cardImgDiv = document.querySelector('.cardImg')
+
+const inputTitulo = document.getElementById('inputTitulo')
+const inputDuracao = document.getElementById('inputDuracao')
+const inputQtdVoluntarios = document.getElementById('inputQtdVoluntarios')
+const inputQtdItensDoarem = document.getElementById('inputQtdItensDoados')
+const inputData = document.getElementById('inputData')
+const inputTempoDescanso = document.getElementById('inputTempoDescanso')
+const inputTipoDoacao = document.getElementById('inputTipoDoacao')
+const inputBeneficiosDoadores = document.getElementById('inputBeneficios')
+
+const textDescricao = document.getElementById('cardTextDescricao')
+const botaoCriarCard = document.getElementById('btnCriarCard')
+
+export{
+    imagemUpload,
+    imagemPreview,
+    textUpload,
+    cardImgDiv,
+    inputTitulo,
+    inputDuracao,
+    inputQtdVoluntarios,
+    inputQtdItensDoarem,
+    inputData,
+    inputTempoDescanso,
+    inputTipoDoacao,
+    inputBeneficiosDoadores,
+    textDescricao
+}
