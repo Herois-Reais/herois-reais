3fb8cb5b85b601f7ca22a6e3964e4a45898e306f
Carla Santos <carla169gama@gmail.com>
2025-11-20 21:26:08 -0300

    home-cadastro

---
+++ home-cadastro/script.js
@@ -0,0 +1,38 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const btnCriarConta = document.getElementById('btn-criar-conta');
+    const modalSelecao = document.getElementById('modalselecao');
+    const btnONG = document.getElementById('btn-ong');
+    const btnDoador = document.getElementById('btn-doador');
+
+    if (modalSelecao) {
+        const modalClose = modalSelecao.querySelector('.modalclose');
+        const modalBackground = modalSelecao.querySelector('.modal-background');
+
+        const abrirModal = () => modalSelecao.classList.add('is-active');
+        const fecharModal = () => modalSelecao.classList.remove('is-active');
+
+        if (btnCriarConta) btnCriarConta.addEventListener('click', abrirModal);
+        if (modalClose) modalClose.addEventListener('click', fecharModal);
+        if (modalBackground) modalBackground.addEventListener('click', fecharModal);
+        
+        } else {
+        if (btnCriarConta) {
+            btnCriarConta.addEventListener('click', (e) => {
+                e.preventDefault();
+                window.location.href = 'cadastro-doador.html';
+            });
+        }
+    }
+});
+
+
+
+
+
+
+
+
+
+    
+
